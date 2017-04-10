844c77814334d5ab288f92685fac2ec06b45a6c7
flisiq <flisiq@gmail.com>
2017-04-10 21:00:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/zoozolpetstore/MapsActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.zoozolpetstore;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.example.android.zoozolpetstore.R.id.map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng storeLoc = new LatLng(51.7179096, 19.4747525);
+        mMap.addMarker(new MarkerOptions().position(storeLoc).title("ZOOZOL Pet Store"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(storeLoc));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+
+
+
+
+    }
+
+}
